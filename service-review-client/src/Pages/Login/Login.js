eb8aec4947fde55dab59481c05a804da4c3b89fc
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-11 00:58:11 +0600

    Basic Login page setup

---
+++ service-review-client/src/Pages/Login/Login.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="hero  bg-base-200">
+      <div className="card  w-1/2  shadow-2xl bg-base-100">
+        <form onSubmit={handleLogin} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="email"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="password"
+              className="input input-bordered"
+            />
+            <label className="label">
+              <a href="/ " className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <input className="btn btn-primary" type="submit" value="Login" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
