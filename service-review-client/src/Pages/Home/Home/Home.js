89eec17211caa541688f900dfbf95cb9cb70235c
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-11 22:51:46 +0600

    Photo view implemented

--- service-review-client/src/Pages/Home/Home/Home.js
+++ service-review-client/src/Pages/Home/Home/Home.js
@@ -1,20 +1,9 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 import Banner from "../Banner/Banner";
-import AddServices from "../Services/AddServices";
 import Services from "../Services/Services";
 
 const Home = () => {
-  const { user } = useContext(AuthContext);
 
-  const addService = () => {
-    if (user) {
-      <Link to="/addServices">
-        <button className="btn btn-primary">Add Services</button>
-      </Link>;
-    }
-  };
+  
 
   return (
     <div>
--- service-review-client/src/Pages/Home/Services/ServiceCard.js
+++ service-review-client/src/Pages/Home/Services/ServiceCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 const ServiceCard = ({ service }) => {
   const { _id, img, title, price } = service;
@@ -7,7 +9,11 @@ const ServiceCard = ({ service }) => {
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
       <figure>
-        <img src={img} alt="car" />
+        <PhotoProvider>
+          <PhotoView src={img}>
+            <img src={img} alt="car" />
+          </PhotoView>
+        </PhotoProvider>
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
