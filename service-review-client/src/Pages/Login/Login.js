0ee4d943885a012391bb7599c6d4e340f8f450a4
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-11 13:42:48 +0600

    Sign up and Login setup

--- service-review-client/src/Pages/Login/Login.js
+++ service-review-client/src/Pages/Login/Login.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
 const Login = () => {
+  const { signIn } = useContext(AuthContext);
+
   const handleLogin = (event) => {
     event.preventDefault();
+    const form = event.target;
+    // const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((err) => console.error(err));
   };
 
   return (
